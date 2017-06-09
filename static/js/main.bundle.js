00991ed9b0468abdc2392ad8f51a6ec8705799c6
Matt Hong <mhong19414@gmail.com>
2017-06-09 10:37:19 -0400

    hmm

--- static/js/main.bundle.js
+++ static/js/main.bundle.js
@@ -36959,10 +36959,10 @@
 			var _this = _possibleConstructorReturn(this, (LinkItem.__proto__ || Object.getPrototypeOf(LinkItem)).call(this, props));
 
 			_this.imgImports = {
-				small: 'https://s3.amazonaws.com/chartrow' + (JSON.stringify((undefined)) === "production" ? "" : "-test") + '/' + _this.props.imgSrc + '.png'
+				small: 'https://s3.amazonaws.com/chartrow' + ((undefined) === "production" ? "" : "-test") + '/' + _this.props.imgSrc + '.png'
 			};
 			if (_this.props.lead) {
-				_this.imgImports.large = 'https://s3.amazonaws.com/chartrow' + (JSON.stringify((undefined)) === "production" ? "" : "-test") + '/' + _this.props.imgSrc + '-400.png';
+				_this.imgImports.large = 'https://s3.amazonaws.com/chartrow' + ((undefined) === "production" ? "" : "-test") + '/' + _this.props.imgSrc + '-400.png';
 			}
 			_this.state = {
 				small: _this.props.small || !_this.props.lead || _this.props.index
